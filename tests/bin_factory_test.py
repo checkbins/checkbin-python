f195a9ffd3dc00bf9ef5b3337f6d3965b93e1b02
timl <tim.lenardo@gmail.com>
2025-01-17 20:03:22 -0800

    remove token

--- tests/bin_factory_test.py
+++ tests/bin_factory_test.py
@@ -8,7 +8,7 @@ company_id = "123"
 question_id = "919"
 short_uuid = str(uuid.uuid4())[:6]
 
-checkbin.authenticate(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOiJiN2VmODczOC0wNzRmLTRjYTgtYWM5Zi1hY2E1OTIwM2RiNDkiLCJpYXQiOjE3MzcxNzI2MDh9.dtbA42PyMT2jcTzUcXeEzcuPYvThV4C3tBVueaI2jfE")
+checkbin.authenticate(token=os.environ["CHECKBIN_TOKEN"])
 checkbin_app = checkbin.App(app_key="testing_dedup", mode="remote")
 bin_factory = checkbin_app.create_bin_factory(run_name=f"{company_id}_{question_id}_{short_uuid}")
 checkbin = bin_factory.get_bin(input_state={"company_id": company_id, "question_id": question_id}, input_files={})
